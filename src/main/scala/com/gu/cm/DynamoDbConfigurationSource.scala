5c96be85ffba73efdb1005f2848d61cd793cd653
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2016-02-10 17:36:54 +0000

    Testing the dynamodb code

--- src/main/scala/com/gu/cm/DynamoDbConfigurationSource.scala
+++ src/main/scala/com/gu/cm/DynamoDbConfigurationSource.scala
@@ -1,20 +1,13 @@
 package com.gu.cm
 
-import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.regions.Region
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.document.{TableKeysAndAttributes, PrimaryKey, DynamoDB}
 import com.typesafe.config.{ConfigFactory, Config}
 import scala.collection.JavaConverters._
 
-class DynamoDbConfigurationSource(credProvider: AWSCredentialsProvider, region: Region, identity: Identity, prefix: String = "config-") extends ConfigurationSource {
-
-  lazy val dynamoDb = {
-    val client = new AmazonDynamoDBClient(credProvider)
-    client.setRegion(region)
-    new DynamoDB(client)
-  }
-
+class DynamoDbConfigurationSource(dynamoDb: DynamoDB, identity: Identity, prefix: String) extends ConfigurationSource {
   override def load: Config = {
     val tableName = s"$prefix${identity.stack}"
     val primaryKey = new PrimaryKey("App", identity.app, "Stage", identity.stage)
@@ -27,5 +20,16 @@ class DynamoDbConfigurationSource(credProvider: AWSCredentialsProvider, region: 
       case Nil => ConfigFactory.empty(s"no DynamoDB config for $tableName [App=${identity.app}, Stage=${identity.stage}]")
       case item :: _ => ConfigFactory.parseMap(item.getMap("Config"), s"Dynamo DB table $tableName [App=${identity.app}, Stage=${identity.stage}]")
     }
+  }
+}
+
+object DynamoDbConfigurationSource {
+  def apply(region: Region, identity: Identity, prefix: String = "config-"): DynamoDbConfigurationSource = {
+    val dynamoDb = {
+      val client = new AmazonDynamoDBClient(new DefaultAWSCredentialsProviderChain())
+      client.setRegion(region)
+      new DynamoDB(client)
+    }
+    new DynamoDbConfigurationSource(dynamoDb, identity, prefix)
   }
 }
---
+++ src/test/scala/com/gu/cm/DynamoDbConfigurationSourceSpec.scala
@@ -0,0 +1,60 @@
+package com.gu.cm
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
+import com.amazonaws.services.dynamodbv2.document.{Item, DynamoDB}
+import com.amazonaws.services.dynamodbv2.model._
+import com.typesafe.config.ConfigFactory
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+
+import scala.collection.JavaConverters._
+
+class DynamoDbConfigurationSourceSpec extends Specification {
+  "a dynamo DB configuration Source" should {
+    "load a config from dynamodb" in new DynamoDbScope {
+      val conf = dynamoDbConfigurationSource.load
+
+      conf.getInt("foo.a") shouldEqual 42
+    }
+  }
+
+  trait DynamoDbScope extends Scope {
+    val dynamoDb = {
+      val client = new AmazonDynamoDBClient(new DefaultAWSCredentialsProviderChain())
+      client.setEndpoint("http://localhost:8000")
+      new DynamoDB(client)
+    }
+
+    val identity = Identity(
+      stack = "test-stack",
+      app = "configuration-magic",
+      stage = "test"
+    )
+
+    val dynamoDbConfigurationSource = new DynamoDbConfigurationSource(dynamoDb, identity, "prefix-")
+
+    def initTable() = {
+      val req = new CreateTableRequest(
+        "prefix-test-stack",
+        List(
+          new KeySchemaElement("App", KeyType.HASH),
+          new KeySchemaElement("Stage", KeyType.RANGE)
+        ).asJava).withAttributeDefinitions(
+        new AttributeDefinition("App", "S"),
+        new AttributeDefinition("Stage", "S")
+      ).withProvisionedThroughput(new ProvisionedThroughput(1000L, 1000L))
+
+      val table = dynamoDb.createTable(req)
+
+      val confBlob = ConfigFactory.parseResourcesAnySyntax("sample").root.unwrapped()
+
+      val item = new Item()
+        .withString("App", "configuration-magic")
+        .withString("Stage", "test")
+        .withMap("Config", confBlob)
+      table.putItem(item)
+    }
+    initTable()
+  }
+}
