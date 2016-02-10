0c2726fe1bbf7ae53d41b78bfd04de6423cc882a
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2016-02-10 14:09:41 +0000

    moving configuration sources to a separate file, add simple implementations of the sources and a default list of sources

--- src/main/scala/com/gu/cm/Configuration.scala
+++ src/main/scala/com/gu/cm/Configuration.scala
@@ -2,18 +2,16 @@ package com.gu.cm
 
 import com.typesafe.config._
 
-trait ConfigurationSource {
-  def load: Config
-}
-
-object EmptyConfigurationSource extends ConfigurationSource {
-  def load: Config = ConfigFactory.empty()
-}
-
 class Configuration(sources: List[ConfigurationSource]) extends ConfigurationSource {
   def load: Config = {
     sources.map(_.load).foldLeft(ConfigFactory.empty()) {
       case (agg, source) => agg.withFallback(source)
     }
   }
+}
+
+object Configuration {
+  def default(identity: Identity): List[ConfigurationSource] = List(
+    FileConfigurationSource(s"${System.getProperty("user.home")}/.configuration-magic/${identity.app}")
+  )
 }
