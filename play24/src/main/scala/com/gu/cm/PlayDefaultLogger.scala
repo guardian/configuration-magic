069cef46410eaae80cbe7e90b8c8ad76b8bbb382
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2016-02-26 14:14:32 +0000

    fixes an interesting logger bug

--- play24/src/main/scala/com/gu/cm/PlayDefaultLogger.scala
+++ play24/src/main/scala/com/gu/cm/PlayDefaultLogger.scala
@@ -3,8 +3,8 @@ package com.gu.cm
 import play.api.{Logger => PlayLogger}
 
 object PlayDefaultLogger extends Logger {
-  override def info(message: => String): Unit = PlayLogger.info(s"[Configuration Magic] message")
-  override def warn(message: => String): Unit = PlayLogger.warn(s"[Configuration Magic] message")
-  override def error(message: => String): Unit = PlayLogger.error(s"[Configuration Magic] message")
-  override def error(message: => String, exception: => Throwable): Unit = PlayLogger.error(s"[Configuration Magic] message", exception)
+  override def info(message: => String): Unit = PlayLogger.info(s"[Configuration Magic] $message")
+  override def warn(message: => String): Unit = PlayLogger.warn(s"[Configuration Magic] $message")
+  override def error(message: => String): Unit = PlayLogger.error(s"[Configuration Magic] $message")
+  override def error(message: => String, exception: => Throwable): Unit = PlayLogger.error(s"[Configuration Magic] $message", exception)
 }
