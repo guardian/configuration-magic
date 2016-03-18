f1d449b2e9458cfa4ac681e8424d9f91adc275d1
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2016-03-18 11:59:07 +0000

    A nicer way to integrate logs with Play

--- play24/src/main/scala/com/gu/cm/PlayDefaultLogger.scala
+++ play24/src/main/scala/com/gu/cm/PlayDefaultLogger.scala
@@ -3,8 +3,9 @@ package com.gu.cm
 import play.api.{Logger => PlayLogger}
 
 object PlayDefaultLogger extends Logger {
-  override def info(message: => String): Unit = PlayLogger.info(s"[Configuration Magic] $message")
-  override def warn(message: => String): Unit = PlayLogger.warn(s"[Configuration Magic] $message")
-  override def error(message: => String): Unit = PlayLogger.error(s"[Configuration Magic] $message")
-  override def error(message: => String, exception: => Throwable): Unit = PlayLogger.error(s"[Configuration Magic] $message", exception)
+  val delegateLogger = PlayLogger("configuration-magic")
+  override def info(message: => String): Unit = delegateLogger.info(message)
+  override def warn(message: => String): Unit = delegateLogger.warn(message)
+  override def error(message: => String): Unit = delegateLogger.error(message)
+  override def error(message: => String, exception: => Throwable): Unit = delegateLogger.error(message, exception)
 }
