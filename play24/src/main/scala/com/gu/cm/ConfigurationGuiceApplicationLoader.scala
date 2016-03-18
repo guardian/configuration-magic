0ba1efe324235b851b96f70a88c6e52d1fe0bbb3
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2016-03-18 11:56:29 +0000

    extract the main integration logic into a reusable unit to ease custom implementations

--- play24/src/main/scala/com/gu/cm/ConfigurationGuiceApplicationLoader.scala
+++ play24/src/main/scala/com/gu/cm/ConfigurationGuiceApplicationLoader.scala
@@ -1,9 +1,7 @@
 package com.gu.cm
 
-import play.api.{Configuration => PlayConfiguration}
 import play.api.ApplicationLoader.Context
 import play.api.inject.guice.{GuiceApplicationBuilder, GuiceApplicationLoader}
-import PlayImplicits._
 
 class ConfigurationGuiceApplicationLoader extends GuiceApplicationLoader() {
 
@@ -11,12 +9,7 @@ class ConfigurationGuiceApplicationLoader extends GuiceApplicationLoader() {
     .getOrElse(throw new RuntimeException("Please define a default application name in application.conf with the property play.application.name"))
 
   override protected def builder(context: Context): GuiceApplicationBuilder = {
-    val config = Configuration(
-      defaultAppName = appName(context),
-      mode = context.environment.mode,
-      logger = PlayDefaultLogger
-    ).load
-    PlayDefaultLogger.info(s"Configuration loaded from ${config.origin().description()}")
-    super.builder(context).loadConfig(context.initialConfiguration ++ PlayConfiguration(config))
+    val contextWithConfiguration = ContextConfigurationLoader(appName(context), context)
+    super.builder(contextWithConfiguration)
   }
 }
