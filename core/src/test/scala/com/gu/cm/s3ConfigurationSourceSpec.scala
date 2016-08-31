75905c5aa5fae8114653b9fe5c350dd689b3e4a2
Dominic Kendrick <dominic.kendrick@guardian.co.uk>
2016-08-31 10:25:04 +0100

    removing unused dependecies

--- core/src/test/scala/com/gu/cm/s3ConfigurationSourceSpec.scala
+++ core/src/test/scala/com/gu/cm/s3ConfigurationSourceSpec.scala
@@ -1,27 +1,11 @@
 package com.gu.cm
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.regions.RegionUtils
 import com.amazonaws.regions.ServiceAbbreviations._
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
-import com.amazonaws.services.dynamodbv2.document.DynamoDB
-import com.amazonaws.services.dynamodbv2.document.Item
-import com.amazonaws.services.dynamodbv2.document.{Item, DynamoDB}
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition
-import com.amazonaws.services.dynamodbv2.model.CreateTableRequest
-import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
-import com.amazonaws.services.dynamodbv2.model.KeyType
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
-import com.amazonaws.services.dynamodbv2.model._
 import com.amazonaws.services.s3.AmazonS3Client
-import com.typesafe.config.ConfigFactory
-import com.typesafe.config.ConfigFactory
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
-
-import scala.collection.JavaConverters._
 
 class s3ConfigurationSourceSpec extends Specification {
   "a s3 configuration Source" should {
